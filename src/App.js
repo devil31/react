9ca5a5ae8791129948eb3b6c199ee8d6cffa8fee
devil31 <37255268+devil31@users.noreply.github.com>
2021-01-26 18:23:09 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+function Ciao(){
+  return (
+    <div>
+      <h1>ciao mondo</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
             ciao mondo 
+            <Ciao/>
       </header>
     </div>
   );
