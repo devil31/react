b3098ac6a78d28ae2981ac75f06fd27c852d5728
devil31 <37255268+devil31@users.noreply.github.com>
2021-01-26 23:53:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-function Ciao(){
-  return (
-    <div>
-      <h1>ciao mondo</h1>
-    </div>
-  )
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ciao, mondo!</h1>
+        <h2>Sono le {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function App() {
@@ -13,7 +39,7 @@ function App() {
     <div className="App">
       <header className="App-header">
             ciao mondo 
-            <Ciao/>
+            <Clock/>
       </header>
     </div>
   );
